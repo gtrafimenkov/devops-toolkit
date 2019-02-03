433cb9a04fea83d1ca1b76162394a7a8539a2ba0
Gennady Trafimenkov <gennady.trafimenkov@gmail.com>
2019-02-04 02:15:58 +0300

    Useful docker images

--- scripts/add-spdx-identifiers.sh
+++ scripts/add-spdx-identifiers.sh
@@ -2,6 +2,13 @@
 
 SD=$(dirname $0)
 
+# docker
+for f in $(find $SD/../docker-images -type f \( -name 'Dockerfile*' -o -name 'Makefile' \) -print); do
+    if ! grep -q "# SPDX-License-Identifier" $f; then
+        sed -i '1s!^!# SPDX-License-Identifier: MIT\n# Copyright (c) 2019 Gennady Trafimenkov\n\n!' $f
+    fi
+done
+
 # ansible
 for f in $(find $SD/../ansible-roles -type f -name '*.yml' -print); do
     if ! grep -q "# SPDX-License-Identifier" $f; then
